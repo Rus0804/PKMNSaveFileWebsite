529b51eb4ebc4c2cd43bd1d1329fabdff1703405
Rus0804 <arushajith0804@gmail.com>
2025-05-11 19:03:29 +0530

    basic ability data

---
+++ frontend/src/data/ability_data.js
@@ -0,0 +1,382 @@
+export const ability_data = {
+  "1": {
+    "id": 1,
+    "name": "Stench",
+    "effect": "Has a 10% chance of making target Pokemon flinch with each hit."
+  },
+  "2": {
+    "id": 2,
+    "name": "Drizzle",
+    "effect": "Summons rain that lasts indefinitely upon entering battle."
+  },
+  "3": {
+    "id": 3,
+    "name": "Speed Boost",
+    "effect": "Raises Speed one stage after each turn."
+  },
+  "4": {
+    "id": 4,
+    "name": "Battle Armor",
+    "effect": "Protects against critical hits."
+  },
+  "5": {
+    "id": 5,
+    "name": "Sturdy",
+    "effect": "Prevents being KOed from full HP, leaving 1 HP instead.  Protects against the one-hit KO moves regardless of HP."
+  },
+  "6": {
+    "id": 6,
+    "name": "Damp",
+    "effect": "Prevents self destruct, explosion, and aftermath from working while the Pokemon is in battle."
+  },
+  "7": {
+    "id": 7,
+    "name": "Limber",
+    "effect": "Prevents paralysis."
+  },
+  "8": {
+    "id": 8,
+    "name": "Sand Veil",
+    "effect": "Increases evasion to 1.25x during a sandstorm.  Protects against sandstorm damage."
+  },
+  "9": {
+    "id": 9,
+    "name": "Static",
+    "effect": "Has a 30% chance of paralyzing attacking Pokemon on contact."
+  },
+  "10": {
+    "id": 10,
+    "name": "Volt Absorb",
+    "effect": "Absorbs electric moves, healing for 1/4 max HP."
+  },
+  "11": {
+    "id": 11,
+    "name": "Water Absorb",
+    "effect": "Absorbs water moves, healing for 1/4 max HP."
+  },
+  "12": {
+    "id": 12,
+    "name": "Oblivious",
+    "effect": "Prevents infatuation and protects against captivate."
+  },
+  "13": {
+    "id": 13,
+    "name": "Cloud Nine",
+    "effect": "Negates all effects of weather, but does not prevent the weather itself."
+  },
+  "14": {
+    "id": 14,
+    "name": "Compound Eyes",
+    "effect": "Increases moves' accuracy to 1.3x."
+  },
+  "15": {
+    "id": 15,
+    "name": "Insomnia",
+    "effect": "Prevents sleep."
+  },
+  "16": {
+    "id": 16,
+    "name": "Color Change",
+    "effect": "Changes type to match when hit by a damaging move."
+  },
+  "17": {
+    "id": 17,
+    "name": "Immunity",
+    "effect": "Prevents poison."
+  },
+  "18": {
+    "id": 18,
+    "name": "Flash Fire",
+    "effect": "Protects against fire moves.  Once one has been blocked, the Pokemon's own Fire moves inflict 1.5x damage until it leaves battle."
+  },
+  "19": {
+    "id": 19,
+    "name": "Shield Dust",
+    "effect": "Protects against incoming moves' extra effects."
+  },
+  "20": {
+    "id": 20,
+    "name": "Own Tempo",
+    "effect": "Prevents confusion."
+  },
+  "21": {
+    "id": 21,
+    "name": "Suction Cups",
+    "effect": "Prevents being forced out of battle by other Pokemon's moves."
+  },
+  "22": {
+    "id": 22,
+    "name": "Intimidate",
+    "effect": "Lowers opponents' Attack one stage upon entering battle."
+  },
+  "23": {
+    "id": 23,
+    "name": "Shadow Tag",
+    "effect": "Prevents opponents from fleeing or switching out."
+  },
+  "24": {
+    "id": 24,
+    "name": "Rough Skin",
+    "effect": "Damages attacking Pokemon for 1/8 their max HP on contact."
+  },
+  "25": {
+    "id": 25,
+    "name": "Wonder Guard",
+    "effect": "Protects against damaging moves that are not super effective."
+  },
+  "26": {
+    "id": 26,
+    "name": "Levitate",
+    "effect": "Evades ground moves."
+  },
+  "27": {
+    "id": 27,
+    "name": "Effect Spore",
+    "effect": "Has a 30% chance of inflcting either paralysis, poison, or sleep on attacking Pokemon on contact."
+  },
+  "28": {
+    "id": 28,
+    "name": "Synchronize",
+    "effect": "Copies burns, paralysis, and poison received onto the Pokemon that inflicted them."
+  },
+  "29": {
+    "id": 29,
+    "name": "Clear Body",
+    "effect": "Prevents stats from being lowered by other Pokemon."
+  },
+  "30": {
+    "id": 30,
+    "name": "Natural Cure",
+    "effect": "Cures any major status ailment upon switching out."
+  },
+  "31": {
+    "id": 31,
+    "name": "Lightning Rod",
+    "effect": "Redirects single-target electric moves to this Pokemon where possible.  Absorbs Electric moves, raising Special Attack one stage."
+  },
+  "32": {
+    "id": 32,
+    "name": "Serene Grace",
+    "effect": "Doubles the chance of moves' extra effects occurring."
+  },
+  "33": {
+    "id": 33,
+    "name": "Swift Swim",
+    "effect": "Doubles Speed during rain."
+  },
+  "34": {
+    "id": 34,
+    "name": "Chlorophyll",
+    "effect": "Doubles Speed during strong sunlight."
+  },
+  "35": {
+    "id": 35,
+    "name": "Illuminate",
+    "effect": "Doubles the wild encounter rate."
+  },
+  "36": {
+    "id": 36,
+    "name": "Trace",
+    "effect": "Copies an opponent's ability upon entering battle."
+  },
+  "37": {
+    "id": 37,
+    "name": "Huge Power",
+    "effect": "Doubles Attack in battle."
+  },
+  "38": {
+    "id": 38,
+    "name": "Poison Point",
+    "effect": "Has a 30% chance of poisoning attacking Pokemon on contact."
+  },
+  "39": {
+    "id": 39,
+    "name": "Inner Focus",
+    "effect": "Prevents flinching."
+  },
+  "40": {
+    "id": 40,
+    "name": "Magma Armor",
+    "effect": "Prevents freezing."
+  },
+  "41": {
+    "id": 41,
+    "name": "Water Veil",
+    "effect": "Prevents burns."
+  },
+  "42": {
+    "id": 42,
+    "name": "Magnet Pull",
+    "effect": "Prevents steel opponents from fleeing or switching out."
+  },
+  "43": {
+    "id": 43,
+    "name": "Soundproof",
+    "effect": "Protects against sound-based moves."
+  },
+  "44": {
+    "id": 44,
+    "name": "Rain Dish",
+    "effect": "Heals for 1/16 max HP after each turn during rain."
+  },
+  "45": {
+    "id": 45,
+    "name": "Sand Stream",
+    "effect": "Summons a sandstorm that lasts indefinitely upon entering battle."
+  },
+  "46": {
+    "id": 46,
+    "name": "Pressure",
+    "effect": "Increases the PP cost of moves targetting the Pokemon by one."
+  },
+  "47": {
+    "id": 47,
+    "name": "Thick Fat",
+    "effect": "Halves damage from fire and ice moves."
+  },
+  "48": {
+    "id": 48,
+    "name": "Early Bird",
+    "effect": "Makes sleep pass twice as quickly."
+  },
+  "49": {
+    "id": 49,
+    "name": "Flame Body",
+    "effect": "Has a 30% chance of burning attacking Pokemon on contact."
+  },
+  "50": {
+    "id": 50,
+    "name": "Run Away",
+    "effect": "Ensures success fleeing from wild battles."
+  },
+  "51": {
+    "id": 51,
+    "name": "Keen Eye",
+    "effect": "Prevents accuracy from being lowered."
+  },
+  "52": {
+    "id": 52,
+    "name": "Hyper Cutter",
+    "effect": "Prevents Attack from being lowered by other Pokemon."
+  },
+  "53": {
+    "id": 53,
+    "name": "Pickup",
+    "effect": "Picks up other Pokemon's used and Flung held items.  May also pick up an item after battle."
+  },
+  "54": {
+    "id": 54,
+    "name": "Truant",
+    "effect": "Skips every second turn."
+  },
+  "55": {
+    "id": 55,
+    "name": "Hustle",
+    "effect": "Strengthens physical moves to inflict 1.5x damage, but decreases their accuracy to 0.8x."
+  },
+  "56": {
+    "id": 56,
+    "name": "Cute Charm",
+    "effect": "Has a 30% chance of infatuating attacking Pokemon on contact."
+  },
+  "57": {
+    "id": 57,
+    "name": "Plus",
+    "effect": "Increases Special Attack to 1.5x when a friendly Pokemon has plus or minus."
+  },
+  "58": {
+    "id": 58,
+    "name": "Minus",
+    "effect": "Increases Special Attack to 1.5x when a friendly Pokemon has plus or minus."
+  },
+  "59": {
+    "id": 59,
+    "name": "Forecast",
+    "effect": "Changes castform's type and form to match the weather."
+  },
+  "60": {
+    "id": 60,
+    "name": "Sticky Hold",
+    "effect": "Prevents a held item from being removed by other Pokemon."
+  },
+  "61": {
+    "id": 61,
+    "name": "Shed Skin",
+    "effect": "Has a 33% chance of curing any major status ailment after each turn."
+  },
+  "62": {
+    "id": 62,
+    "name": "Guts",
+    "effect": "Increases Attack to 1.5x with a major status ailment."
+  },
+  "63": {
+    "id": 63,
+    "name": "Marvel Scale",
+    "effect": "Increases Defense to 1.5x with a major status ailment."
+  },
+  "64": {
+    "id": 64,
+    "name": "Liquid Ooze",
+    "effect": "Damages opponents using leeching moves for as much as they would heal."
+  },
+  "65": {
+    "id": 65,
+    "name": "Overgrow",
+    "effect": "Strengthens grass moves to inflict 1.5x damage at 1/3 max HP or less."
+  },
+  "66": {
+    "id": 66,
+    "name": "Blaze",
+    "effect": "Strengthens fire moves to inflict 1.5x damage at 1/3 max HP or less."
+  },
+  "67": {
+    "id": 67,
+    "name": "Torrent",
+    "effect": "Strengthens water moves to inflict 1.5x damage at 1/3 max HP or less."
+  },
+  "68": {
+    "id": 68,
+    "name": "Swarm",
+    "effect": "Strengthens bug moves to inflict 1.5x damage at 1/3 max HP or less."
+  },
+  "69": {
+    "id": 69,
+    "name": "Rock Head",
+    "effect": "Protects against recoil damage."
+  },
+  "70": {
+    "id": 70,
+    "name": "Drought",
+    "effect": "Summons strong sunlight that lasts indefinitely upon entering battle."
+  },
+  "71": {
+    "id": 71,
+    "name": "Arena Trap",
+    "effect": "Prevents opponents from fleeing or switching out.  Eluded by flying-types and Pokemon in the air."
+  },
+  "72": {
+    "id": 72,
+    "name": "Vital Spirit",
+    "effect": "Prevents sleep."
+  },
+  "73": {
+    "id": 73,
+    "name": "White Smoke",
+    "effect": "Prevents stats from being lowered by other Pokemon."
+  },
+  "74": {
+    "id": 74,
+    "name": "Pure Power",
+    "effect": "Doubles Attack in battle."
+  },
+  "75": {
+    "id": 75,
+    "name": "Shell Armor",
+    "effect": "Protects against critical hits."
+  },
+  "76": {
+    "id": 76,
+    "name": "Air Lock",
+    "effect": "Negates all effects of weather, but does not prevent the weather itself."
+  }
+}
