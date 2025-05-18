304a96f7d0f8d7097eb105fd5c9b007c071c53c5
Rus0804 <arushajith0804@gmail.com>
2025-05-18 12:38:19 +0530

    accessing  mistake

--- backend/login_auth.py
+++ backend/login_auth.py
@@ -92,10 +92,10 @@ def update_save(save_id: int, col, data, request: Request, change):
     try:
         if(col == 'save_data' and change != 'all'):
             old_row = user_db.from_("Saves").select(col).eq("id",save_id).execute()
-            print("old_data: ", old_row)
+            print("old_data: ", old_row['save_data'])
             if(change == 'trainer'):
-                old_row[change]['badges'] = data
-                data = old_row
+                old_row['save_data'][change]['badges'] = data
+                data = old_row['save_data']
         user_db.from_("Saves").update({
             col: data
         }).eq("id", save_id).execute()
