7e5a73c2e02611e9617f02f54d0373802ab5aebe
Rus0804 <arushajith0804@gmail.com>
2025-05-19 19:03:31 +0530

    empty boxes no more

--- frontend/src/components/pokemoninfo/PCBoxes.js
+++ frontend/src/components/pokemoninfo/PCBoxes.js
@@ -11,31 +11,37 @@ function PCBoxes({ boxes, onCardClick}) {
   return (
     <section>
       <h2>PC Boxes</h2>
-      {Object.entries(boxes).map(([boxNum, mons]) => (
-        <div key={boxNum} className="pc-box">
-          <h3>Box {boxNum}</h3>
-          <div className="pc-grid">
-            {mons.map((mon, idx) => (
-              <div
-                className="pc-card"
-                key={idx}
-                onClick={() => onCardClick(mon)} // Trigger side panel on click
-                style={{ cursor: mon ? 'pointer' : 'default' }}
-              >
-                <img
-                  src={mon.shiny?`/Sprites/Pokemon/BW/shiny/${mon.pokedex_num}s.png`:`/Sprites/Pokemon/BW/${mon.pokedex_num}.png`}
-                  alt={mon.nickname || mon.name}
-                  className="pc-sprite"
-                />
-                <h4>{mon.nickname || mon.species}</h4>
-                <p><strong>Lv:</strong> {mon.level}</p>
-                <p><strong>Nature:</strong> {mon.nature}</p>
-                <p><strong>Held Item:</strong> {mon.held_item === 0 ? 'None' : item_data.find(item => item.id === mon.held_item).name}</p>
-              </div>
-            ))}
+
+      {Object.entries(boxes).map(([boxNum, mons]) => {
+        if(mons.length>0){
+          return(
+          <div key={boxNum} className="pc-box">
+            <h3>Box {boxNum}</h3>
+            <div className="pc-grid">
+              {mons.map((mon, idx) => (
+                <div
+                  className="pc-card"
+                  key={idx}
+                  onClick={() => onCardClick(mon)} // Trigger side panel on click
+                  style={{ cursor: mon ? 'pointer' : 'default' }}
+                >
+                  <img
+                    src={mon.shiny?`/Sprites/Pokemon/BW/shiny/${mon.pokedex_num}s.png`:`/Sprites/Pokemon/BW/${mon.pokedex_num}.png`}
+                    alt={mon.nickname || mon.name}
+                    className="pc-sprite"
+                  />
+                  <h4>{mon.nickname || mon.species}</h4>
+                  <p><strong>Lv:</strong> {mon.level}</p>
+                  <p><strong>Nature:</strong> {mon.nature}</p>
+                  <p><strong>Held Item:</strong> {mon.held_item === 0 ? 'None' : item_data.find(item => item.id === mon.held_item).name}</p>
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-      ))}
+          )
+        }
+        else{return null}       
+      })}
     </section>
   );
 }
