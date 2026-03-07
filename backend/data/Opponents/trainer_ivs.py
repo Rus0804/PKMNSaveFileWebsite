c661fa8d158adc0316541ec80abeefbb69665d6a
Arush-goML <arushajith0804@gmail.com>
2026-03-07 11:22:39 +0530

    Changed location of data files

---
+++ backend/data/Opponents/trainer_ivs.py
@@ -0,0 +1,232 @@
+standard_frlg = {
+    "Pokemaniac":30,
+    "Psychic": 50,
+    "Tamer": 40,
+    "Black Belt": 100,
+    "22 Rival": 50,
+    "Giovanni": 250,
+    "Cooltrainer": 100,
+    "Elite 4": 250,
+    "Tower Rival": 100,
+    "Champ" : 255,
+    "Cool Couple": 100,
+    "Crush": 50,
+    "Pokemon Breeder": 30,
+    "Pokemon Ranger" : 100,
+    "Painter": 50,
+    "Team Rocket Admin 1": 150,
+    "Team Rocket Admin 2": 200,
+    "Elite 4 Rematch": 255,
+    }
+
+ace_frlg = {
+    "24 Rival": [50, 50 , 50, 100],
+    "S.S Anne Rival": [50, 50 , 50, 100],
+    "Silph Rival": [100, 100, 100, 100, 150],
+    "22 Rival 2": [150, 150, 150, 150, 150, 250]
+    }
+
+exceptions_frlg = {
+    "Psychic Laura": 100,
+    "Psychic Jaclyn": 100,
+    "Psychic Rodette": 100,
+    "Psychic Dario": 100,
+    "Cue Ball Paxton": 50
+    }
+
+standard_emerald = {
+    "Hiker": 0,
+    "Cooltrainer": 100,
+    "Bird Keeper": 0,
+    "Collector": 0,
+    "Swimmer": 0,
+    "Expert": 0,
+    "Black Belt": 100,
+    "Hex Maniac": 0,
+    "Aroma Lady": 0,
+    "Ruin Maniac": 50,
+    "Interviewer": 50,
+    "Tuber": 0,
+    "Lady": 0,
+    "Beauty": 0
+
+}
+
+ace_emerald = {
+
+}
+
+exceptions_emerald = {
+
+}
+
+emerald_trainer_ivs = {
+    ## Hiker 10/22
+    "Hiker Eli": 100,
+    "Hiker Marc": [120, 130],
+    "Hiker Trent [2]": 10,
+    "Hiker Trent [3]": 20,
+    "Hiker Trent [4]": 30,
+    "Hiker Trent [5]": 40,
+    "Hiker Sawyer [2]": 10,
+    "Hiker Sawyer [3]": 20,
+    "Hiker Sawyer [4]": 30,
+    "Hiker Sawyer [5]": 40,
+
+    ## Cooltrainer 28/56
+    "Cooltrainer Randall": 255,
+    "Cooltrainer Parker": 255,
+    "Cooltrainer George": 255,
+    "Cooltrainer Berke": 255,
+    "Cooltrainer Wilton [2]": 110,
+    "Cooltrainer Wilton [3]": 120,
+    "Cooltrainer Wilton [4]": 130,
+    "Cooltrainer Wilton [5]": 140,
+    "Cooltrainer Mary": 255,
+    "Cooltrainer Alexia": 255,
+    "Cooltrainer Jody": 255,
+    "Cooltrainer Jennifer": 200,
+    "Cooltrainer Brooke [2]": 110,
+    "Cooltrainer Brooke [3]": 120,
+    "Cooltrainer Brooke [4]": 130,
+    "Cooltrainer Brooke [5]": 140,
+    "Cooltrainer Dianne": 0,
+    "Cooltrainer Jazmyn": 0,
+    "Cooltrainer Marley": 255,
+    "Cooltrainer Mitchell": 0,
+    "Cooltrainer Halle": 0,
+    "Cooltrainer Jonathan": 0,
+    "Cooltrainer Darcy": 0,
+    "Cooltrainer Carolina": 50,
+    "Cooltrainer Cristin [2]": 110,
+    "Cooltrainer Cristin [3]": 120,
+    "Cooltrainer Cristin [4]": 130,
+    "Cooltrainer Cristin [5]": 140,
+
+    ## Bird Keeper 10/23
+    "Bird Keeper Jared": 100,
+    "Bird Keeper Humberto": 250,
+    "Bird Keeper Edwardo": 150,
+    "Bird Keeper Robert [2]": 10,
+    "Bird Keeper Robert [3]": 20,
+    "Bird Keeper Robert [4]": 30,
+    "Bird Keeper Robert [5]": 40,
+    "Bird Keeper Alex": 150,
+    "Bird Keeper Josue": 50,
+    "Bird Keeper Darius": 200,
+
+    ## Collector 0/7
+
+    ## Swimmer 6/64
+    "Swimmer Douglas": 10,
+    "Swimmer Darrin": 10,
+    "Swimmer Tony [2]": 10,
+    "Swimmer Tony [3]": 20,
+    "Swimmer Tony [4]": 30,
+    "Swimmer Tony [5]": 40,
+
+    ## Expert 13/16
+    "Expert Fredrick": 100,
+    "Expert Mollie": [0, 200],
+    "Expert Timothy": 200,
+    "Expert Timothy [2]": 210,
+    "Expert Timothy [3]": 220,
+    "Expert Timothy [4]": 230,
+    "Expert Timothy [5]": 240,
+    "Expert Shelby": 200,
+    "Expert Shelby [2]": 210,
+    "Expert Shelby [3]": 220,
+    "Expert Shelby [4]": 230,
+    "Expert Shelby [5]": 240,
+    "Expert Conor": [0, 200],
+
+    ## Black Belt 14/20
+    "Black Belt Zander": 0,
+    "Black Belt Takao": 127,
+    "Black Belt Hitoshi": [50, 200],
+    "Black Belt Koichi": [100, 150],
+    "Black Belt Nob [2]": 110,
+    "Black Belt Nob [3]": 120,
+    "Black Belt Nob [4]": 130,
+    "Black Belt Nob [5]": 140,
+    "Black Belt Cristian": 200,
+    "Black Belt Koji": 0,
+    "Black Belt Koji [2]": 10,
+    "Black Belt Koji [3]": 20,
+    "Black Belt Koji [4]": 30,
+    "Black Belt Koji [5]": 40,
+
+    ## Hex Maniac 7/12
+    "Hex Maniac Tasha": 50,
+    "Hex Maniac Valerie [2]": 10,
+    "Hex Maniac Valerie [3]": 20,
+    "Hex Maniac Valerie [4]": 30,
+    "Hex Maniac Valerie [5]": 40,
+    "Hex Maniac Sylvia": 100,
+    "Hex Maniac Kathleen": 100,
+
+    ## Aroma Lady 4/9
+    "Aroma Lady Rose [2]": 10,
+    "Aroma Lady Rose [3]": 20,
+    "Aroma Lady Rose [4]": 30,
+    "Aroma Lady Rose [5]": 40,
+
+    ## Ruin Maniac /14
+    "Ruin Maniac Foster": 100,
+    "Ruin Maniac Dusty [2]": 60,
+    "Ruin Maniac Dusty [3]": 70,
+    "Ruin Maniac Dusty [4]": 80,
+    "Ruin Maniac Dusty [5]": 90,
+    "Ruin Maniac Garrison": 0,
+    "Ruin Maniac Bryan": 0,
+    "Ruin Maniac Andres [2]": 10,
+    "Ruin Maniac Andres [3]": 20,
+    "Ruin Maniac Andres [4]": 30,
+    "Ruin Maniac Andres [5]": 40,
+
+    ## Interviewer 5/6
+    "Interviewer Gabby & Ty [2]": 100,
+    "Interviewer Gabby & Ty [3]": 150,
+    "Interviewer Gabby & Ty [4]": 200,
+    "Interviewer Gabby & Ty [5]": 250,
+    "Interviewer Gabby & Ty [6]": 250,
+
+    ## Tuber 10/17
+    "Tuber Lola": 10,
+    "Tuber Lola [2]": 10,
+    "Tuber Lola [3]": 20,
+    "Tuber Lola [4]": 30,
+    "Tuber Lola [5]": 40,
+    "Tuber Ricky": 10,
+    "Tuber Ricky [2]": 10,
+    "Tuber Ricky [3]": 20,
+    "Tuber Ricky [4]": 30,
+    "Tuber Ricky [5]": 40,
+
+    ## Lady 7/10
+    "Lady Daphne": 100,
+    "Lady Brianna": 150,
+    "Lady Naomi": 100,
+    "Lady Cindy [3]": 10,
+    "Lady Cindy [4]": 20,
+    "Lady Cindy [5]": 30,
+    "Lady Cindy [6]": 40,
+
+    ## Beauty 12/18
+    "Beauty Connie": 100,
+    "Beauty Bridget": 150,
+    "Beauty Olivia": 100,
+    "Beauty Tiffany": 100,
+    "Beauty Jessica [2]": 10,
+    "Beauty Jessica [3]": 20,
+    "Beauty Jessica [4]": 30,
+    "Beauty Jessica [5]": 40,
+    "Beauty Thalia [2]": 10,
+    "Beauty Thalia [3]": 20,
+    "Beauty Thalia [4]": 30,
+    "Beauty Thalia [5]": 40,
+
+    ## 
+
+
+}
